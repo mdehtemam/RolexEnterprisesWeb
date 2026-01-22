3b26e768155380c43ed1cfbd40fb2df40cc98602
md ehtemam <mdehtemam0511@gmail.com>
2026-01-22 22:57:40 +0530

    chore: commit all changes for deployment

--- scripts/checkFiles.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const p = path.join(__dirname, '..', 'src', 'integrations', 'supabase', 'client.ts');
-console.log('Checking path:', p);
-console.log('Exists:', fs.existsSync(p));
-console.log('Stat:', fs.existsSync(p) ? fs.statSync(p) : 'N/A');
